f3c79d3da3edaea8359cb487c95d362f59705667
Leila Adams <leila.n.adams@gmail.com>
2025-04-30 13:18:54 -0400

    whitelist for CORS

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://sci-guard-backend.onrender.com"],  # Adjust to your frontend host
+    allow_origins=["http://localhost:3000", "https://sciguard.netlify.app"],  # Adjust to your frontend host
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
