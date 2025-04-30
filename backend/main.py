8b166a180d4ecfbe1001430577906990395447ec
Leila Adams <leila.n.adams@gmail.com>
2025-04-29 23:00:57 -0400

    413 content too large

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.responses import Response
+
+class LimitPayloadSizeMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        max_body_size = 20 * 1024 * 1024  # 10 MB
+        body = await request.body()
+        if len(body) > max_body_size:
+            return Response("Payload too large", status_code=413)
+        request._body = body
+        return await call_next(request)
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -8,6 +19,7 @@ from routes import rewrite
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(LimitPayloadSizeMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
